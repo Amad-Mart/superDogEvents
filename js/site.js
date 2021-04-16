8be3ea8db14697eb7ee8567eb1272cc0155e9de5
Amadeus Martinez <Amadeusrm97@gmail.com>
2021-04-16 12:18:22 -0400

    dropdown functional, table not anymore?

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
-let addressArray = [{
+let events = [{
         name: "Yukon Potatoes",
         city: "Gainesville",
         state: "Georgia",
@@ -25,67 +25,90 @@ let addressArray = [{
         phone: "777-345-1234",
     }
 ];
-loadAddressBook();
+var filteredEvents = events;
 
-function loadAddressBook() {
-    let addressBook = [];
-    addressBook = getData();
-    displayData(addressBook);
-}
+function buildDropDown() {
+    var eventDD = document.getElementById("eventDropDown");
+    // this creates distinctEvents to a new Set() of elements with .city
+    let distinctEvents = [...new Set(events.map((events) => events.city))];
 
-function getData() {
-    let addressBook = JSON.parse(localStorage.getItem("addressArray")) || [];
+    let linkHTMLEnd = '<div class="dropdown-divider"></div><a class="dropdown-item" onclick="getEvents(this)" data-string="All" >All</a>';
+    let resultsHTML = "";
 
-    if (addressBook.length == 0) {
-        addressBook = address;
-        localStorage.setItem("addressArray", JSON.stringify(addressBook));
+    for (let i = 0; i < distinctEvents.length; i++) {
+        resultsHTML += `<a class="dropdown-item" onclick="getEvents(this)" data-string="${distinctEvents[i]}">${distinctEvents[i]}</a>`;
     }
-    return addressBook;
+    
+    resultsHTML += linkHTMLEnd;
+    eventDD.innerHTML = resultsHTML;
 }
 
-function saveAddress() {
-    //grab the events out of local storage
-    let addressBook = JSON.parse(localStorage.getItem("addressArray")) || addressArray;
-    // create new object
-    let obj = {};
-    //assign that new object new attributes! wow
-    obj["name"] = document.getElementById("newName").value;
-    obj["city"] = document.getElementById("newCity").value;
-    obj["state"] = document.getElementById("newState").value;
-    obj["email"] = document.getElementById("newEmail").value;
-    obj["phone"] = document.getElementById("newPhone").value;
+function getEvents() {
 
-    addressBook.push(obj);
-
-    localStorage.setItem("addressArray", JSON.stringify(addressBook));
-
-    // Access the values fromthe form by ID and add an objecet to the array.
-    displayData(addressBook);
 }
 
-function displayData(addressBook) {
-    const template = document.getElementById("Data-template");
-    const resultsBody = document.getElementById("resultsBody");
-    //clear table first
-    resultsBody.innerHTML = "";
-    for (let i = 0; i < addressBook.length; i++) {
-        const dataRow = document.importNode(template.content, true);
 
-        dataRow.getElementById("name").textContent = addressBook[i].name;
-        dataRow.getElementById("city").textContent = addressBook[i].city;
-        dataRow.getElementById("state").textContent = addressBook[i].state;
-        dataRow.getElementById("email").textContent = addressBook[i].email;
-        dataRow.getElementById("phone").textContent = formatPhoneNumber(addressBook[i].phone);
+// loadAddressBook();
 
-        resultsBody.appendChild(dataRow);
-    }
-}
+// function loadAddressBook() {
+//     let addressBook = [];
+//     addressBook = getData();
+//     displayData(addressBook);
+// }
 
-function formatPhoneNumber(phoneNumberString) {
-    let cleaned = ('' + phoneNumberString).replace(/\D/g, '');
-    let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
-    if (match) {
-        return '(' + match[1] + ') ' + match[2] + '-' + match[3];
-    }
-    return null;
-}
+// function getData() {
+//     let addressBook = JSON.parse(localStorage.getItem("addressArray")) || [];
+
+//     if (addressBook.length == 0) {
+//         addressBook = address;
+//         localStorage.setItem("addressArray", JSON.stringify(addressBook));
+//     }
+//     return addressBook;
+// }
+
+// function saveAddress() {
+//     //grab the events out of local storage
+//     let addressBook = JSON.parse(localStorage.getItem("addressArray")) || addressArray;
+//     // create new object
+//     let obj = {};
+//     //assign that new object new attributes! wow
+//     obj["name"] = document.getElementById("newName").value;
+//     obj["city"] = document.getElementById("newCity").value;
+//     obj["state"] = document.getElementById("newState").value;
+//     obj["email"] = document.getElementById("newEmail").value;
+//     obj["phone"] = document.getElementById("newPhone").value;
+
+//     addressBook.push(obj);
+
+//     localStorage.setItem("addressArray", JSON.stringify(addressBook));
+
+//     // Access the values fromthe form by ID and add an objecet to the array.
+//     displayData(addressBook);
+// }
+
+// function displayData(addressBook) {
+//     const template = document.getElementById("Data-template");
+//     const resultsBody = document.getElementById("resultsBody");
+//     //clear table first
+//     resultsBody.innerHTML = "";
+//     for (let i = 0; i < addressBook.length; i++) {
+//         const dataRow = document.importNode(template.content, true);
+
+//         dataRow.getElementById("name").textContent = addressBook[i].name;
+//         dataRow.getElementById("city").textContent = addressBook[i].city;
+//         dataRow.getElementById("state").textContent = addressBook[i].state;
+//         dataRow.getElementById("email").textContent = addressBook[i].email;
+//         dataRow.getElementById("phone").textContent = formatPhoneNumber(addressBook[i].phone);
+
+//         resultsBody.appendChild(dataRow);
+//     }
+// }
+
+// function formatPhoneNumber(phoneNumberString) {
+//     let cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+//     let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+//     if (match) {
+//         return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+//     }
+//     return null;
+// }
